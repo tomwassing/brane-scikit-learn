11779b8ffa367a8ca656b2244f83e0f01e92bd0e
Jurre Brandsen <jurrebrandsen@gmail.com>
2022-05-19 13:04:32 +0200

    attempting to create PCA

--- run.py
+++ run.py
@@ -5,46 +5,59 @@ import os
 import sys
 import yaml
 import io
+import inspect
 from sklearn.decomposition import PCA
 from sklearn import datasets
-
+import ast
 import numpy as np
 
-def decode(s: str) -> str:
-  s = s.replace("\n", "")
-  b = base64.b64decode(s)
-  return b.decode("UTF-8")
-
-def encode(s: str) -> str:
-  b = s.encode("UTF-8")
-  b = base64.b64encode(b)
-  return b.decode("UTF-8")
 
 if __name__ == "__main__":
   command = sys.argv[1]
-  argument = os.environ["INPUT"]
+  input_data = os.environ["INPUT"]
+  n_components = os.environ.get("N_COMPONENTS")
 
-  if command == "iris":
-    iris = datasets.load_iris()
-    fake_file = io.StringIO()
-    np.savetxt(fake_file, iris.data)
-    # print(yaml.dump({"output": fake_file.getvalue()}))
-  else:
-    # TODO: replace with INPUT
-    iris = datasets.load_iris()
-    fake_file = io.StringIO()
-    np.savetxt(fake_file, iris.data)
-    fake_file2 = io.StringIO(fake_file.getvalue())
-    data = np.loadtxt(fake_file2)
+# if command == "iris":
+  iris = datasets.load_iris()
+  fake_file = io.StringIO()
+  np.savetxt(fake_file, iris.data)
+  input_data = fake_file.getvalue()
 
-    # print(data)
+  input_data = iris.data
 
-    pca  = PCA(n_components=2)
-    method = getattr(pca, command)
-    result = method(data)
+#   # print(yaml.dump({"output": fake_file.getvalue()}))
+# else:
+#   # TODO: replace with INPUT
+#   iris = datasets.load_iris()
+#   fake_file = io.StringIO()
+#   np.savetxt(fake_file, iris.data)
+#   fake_file2 = io.StringIO(fake_file.getvalue())
+#   data = np.loadtxt(fake_file2)
 
-    fake_file = io.StringIO()
-    np.savetxt(fake_file, result)
-    # print(result)
+  # print(data)
 
-    print(yaml.dump({"output": fake_file.getvalue()}))
+  args = dict()
+  signature = inspect.signature(PCA.__init__)
+
+  for param in signature.parameters:
+      if param == "self":
+        continue
+      value = os.environ.get(param.upper())
+      if value is not None:
+        try:
+          args[param] = ast.literal_eval(value)
+        except:
+          args[param] = value
+  # print(args)
+  # print(**args)
+
+  pca = PCA(**args)
+  print(input_data)
+  method = getattr(pca, command)
+  result = method(input_data)
+
+  fake_file = io.StringIO()
+  np.savetxt(fake_file, result)
+  # print(result)
+
+  print(yaml.dump({"output": fake_file.getvalue()}))
