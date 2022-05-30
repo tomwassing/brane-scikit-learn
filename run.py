c8e3535af54f77b5ad5a939b959a54c076b3c5b4
Tom J. Wassing <tjwassing@gmail.com>
2022-05-30 22:27:38 +0200

    Fixed output

--- run.py
+++ run.py
@@ -74,8 +74,7 @@ def main(function_name):
         sys.exit(1)
 
     raw_output = json.dumps(result.tolist())
-    yaml_output = yaml.dump({"output": raw_output})
-    return yaml_output
+    return yaml.dump({"output": raw_output})
 
 
 if __name__ == "__main__":
